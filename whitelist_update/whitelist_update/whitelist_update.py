0f1c336ff3346cc86491ab803a8cd514ca0c7bc8
Erz <lukas.parant@epitech.eu>
2017-02-23 16:44:59 +0100

    quick update - errors

--- whitelist_update/whitelist_update/whitelist_update.py
+++ whitelist_update/whitelist_update/whitelist_update.py
@@ -10,13 +10,16 @@ def gest_error(code):
     return
 
 def find_uuid(name):
-    url = urllib.request.urlopen("http://tools.glowingmines.eu/convertor/nick/" + name)
-    tmp = str(url.read())
-    res = tmp.split(",")[1].split("\"offlineuuid\":")[1].split("\"")[1]
-    res = res[:8] + '-' + res[8:]
-    res = res[:13] + '-' + res[13:]
-    res = res[:18] + '-' + res[18:]
-    res = res[:23] + '-' + res[23:]
+    try:
+        url = urllib.request.urlopen("http://tools.glowingmines.eu/convertor/nick/" + name)
+        tmp = str(url.read())
+        res = tmp.split(",")[1].split("\"offlineuuid\":")[1].split("\"")[1]
+        res = res[:8] + '-' + res[8:]
+        res = res[:13] + '-' + res[13:]
+        res = res[:18] + '-' + res[18:]
+        res = res[:23] + '-' + res[23:]
+    except:
+        return (None)
     return (res)
 
 try:
@@ -24,26 +27,33 @@ try:
 except:
     gest_error("file could not be read")
 
-file_str = f.read()
-lines = file_str.split("\n")
+try:
+    file_str = f.read()
+    lines = file_str.split("\n")
 
-names = []
-uuids = []
-l = 0
-while l < len(lines):
-    if len(lines[l]) > 3:
-        sp = lines[l].split("\"uuid\": ")
-        if (len(sp) > 1):
-            uuid = sp[1].split("\"")[1]
+    names = []
+    uuids = []
+    l = 0
+    while l < len(lines):
+        if len(lines[l]) > 3:
+            sp = lines[l].split("\"uuid\": ")
+            if (len(sp) > 1):
+                uuid = sp[1].split("\"")[1]
+            l += 1
+            sp = lines[l].split("\"name\": ")
+            if (len(sp) > 1):
+                name = sp[1].split("\"")[1]
+                names.append(name)
+                real_uuid = find_uuid(name)
+                if real_uuid == None:
+                    print("could not read uuid for "+ name)
+                    uuids.append(uuid)
+                else:
+                    print(real_uuid)
+                    uuids.append(real_uuid)
         l += 1
-        sp = lines[l].split("\"name\": ")
-        if (len(sp) > 1):
-            name = sp[1].split("\"")[1]
-            names.append(name)
-            real_uuid = find_uuid(name)
-            print(real_uuid)
-            uuids.append(real_uuid)
-    l += 1
+except:
+    gest_error("failed to load file")
 
 f.close()
 
@@ -60,7 +70,9 @@ to_write += "]"
 
 try:
     f = open("whitelist.json", "w")
+    f.write(to_write)
+    f.close()
 except:
-    gest_error("file could not be read")
-f.write(to_write)
-f.close()
+    gest_error("file could not be overwritten")
+
+print("Done.")
